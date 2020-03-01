c8f9fe13c62c513ce0afc4d25750295e2bb972fe
baron2020 <61102173+baron2020@users.noreply.github.com>
2020-03-01 10:42:47 +0900

    Add files via upload

---
+++ js/0130.js
@@ -0,0 +1,514 @@
+﻿
+<!-- imgエレメント生成 -->
+<!-- ハートエレメント作成-->
+var h1=document.createElement("img");
+h1.setAttribute("src","images/heart/heart1.jpg");
+h1.setAttribute("width","100");
+h1.setAttribute("height","100");
+var h2=document.createElement("img");
+h2.setAttribute("src","images/heart/heart2.jpg");
+h2.setAttribute("width","100");
+h2.setAttribute("height","100");
+var h3=document.createElement("img");
+h3.setAttribute("src","images/heart/heart3.jpg");
+h3.setAttribute("width","100");
+h3.setAttribute("height","100");
+var h4=document.createElement("img");
+h4.setAttribute("src","images/heart/heart4.jpg");
+h4.setAttribute("width","100");
+h4.setAttribute("height","100");
+var h5=document.createElement("img");
+h5.setAttribute("src","images/heart/heart5.jpg");
+h5.setAttribute("width","100");
+h5.setAttribute("height","100");
+var h6=document.createElement("img");
+h6.setAttribute("src","images/heart/heart6.jpg");
+h6.setAttribute("width","100");
+h6.setAttribute("height","100");
+var h7=document.createElement("img");
+h7.setAttribute("src","images/heart/heart7.jpg");
+h7.setAttribute("width","100");
+h7.setAttribute("height","100");
+var h8=document.createElement("img");
+h8.setAttribute("src","images/heart/heart8.jpg");
+h8.setAttribute("width","100");
+h8.setAttribute("height","100");
+var h9=document.createElement("img");
+h9.setAttribute("src","images/heart/heart9.jpg");
+h9.setAttribute("width","100");
+h9.setAttribute("height","100");
+var h10=document.createElement("img");
+h10.setAttribute("src","images/heart/heart10.jpg");
+h10.setAttribute("width","100");
+h10.setAttribute("height","100");
+var h11=document.createElement("img");
+h11.setAttribute("src","images/heart/heart11.jpg");
+h11.setAttribute("width","100");
+h11.setAttribute("height","100");
+var h12=document.createElement("img");
+h12.setAttribute("src","images/heart/heart12.jpg");
+h12.setAttribute("width","100");
+h12.setAttribute("height","100");
+var h13=document.createElement("img");
+h13.setAttribute("src","images/heart/heart13.jpg");
+h13.setAttribute("width","100");
+h13.setAttribute("height","100");
+
+<!-- クローバーエレメント作成-->
+var c1=document.createElement("img");
+c1.setAttribute("src","images/clover/clover1.jpg");
+c1.setAttribute("width","100");
+c1.setAttribute("height","100");
+var c2=document.createElement("img");
+c2.setAttribute("src","images/clover/clover2.jpg");
+c2.setAttribute("width","100");
+c2.setAttribute("height","100");
+var c3=document.createElement("img");
+c3.setAttribute("src","images/clover/clover3.jpg");
+c3.setAttribute("width","100");
+c3.setAttribute("height","100");
+var c4=document.createElement("img");
+c4.setAttribute("src","images/clover/clover4.jpg");
+c4.setAttribute("width","100");
+c4.setAttribute("height","100");
+var c5=document.createElement("img");
+c5.setAttribute("src","images/clover/clover5.jpg");
+c5.setAttribute("width","100");
+c5.setAttribute("height","100");
+var c6=document.createElement("img");
+c6.setAttribute("src","images/clover/clover6.jpg");
+c6.setAttribute("width","100");
+c6.setAttribute("height","100");
+var c7=document.createElement("img");
+c7.setAttribute("src","images/clover/clover7.jpg");
+c7.setAttribute("width","100");
+c7.setAttribute("height","100");
+var c8=document.createElement("img");
+c8.setAttribute("src","images/clover/clover8.jpg");
+c8.setAttribute("width","100");
+c8.setAttribute("height","100");
+var c9=document.createElement("img");
+c9.setAttribute("src","images/clover/clover9.jpg");
+c9.setAttribute("width","100");
+c9.setAttribute("height","100");
+var c10=document.createElement("img");
+c10.setAttribute("src","images/clover/clover10.jpg");
+c10.setAttribute("width","100");
+c10.setAttribute("height","100");
+var c11=document.createElement("img");
+c11.setAttribute("src","images/clover/clover11.jpg");
+c11.setAttribute("width","100");
+c11.setAttribute("height","100");
+var c12=document.createElement("img");
+c12.setAttribute("src","images/clover/clover12.jpg");
+c12.setAttribute("width","100");
+c12.setAttribute("height","100");
+var c13=document.createElement("img");
+c13.setAttribute("src","images/clover/clover13.jpg");
+c13.setAttribute("width","100");
+c13.setAttribute("height","100");
+
+<!-- ダイヤエレメント作成-->
+var d1=document.createElement("img");
+d1.setAttribute("src","images/dia/dia1.jpg");
+d1.setAttribute("width","100");
+d1.setAttribute("height","100");
+var d2=document.createElement("img");
+d2.setAttribute("src","images/dia/dia2.jpg");
+d2.setAttribute("width","100");
+d2.setAttribute("height","100");
+var d3=document.createElement("img");
+d3.setAttribute("src","images/dia/dia3.jpg");
+d3.setAttribute("width","100");
+d3.setAttribute("height","100");
+var d4=document.createElement("img");
+d4.setAttribute("src","images/dia/dia4.jpg");
+d4.setAttribute("width","100");
+d4.setAttribute("height","100");
+var d5=document.createElement("img");
+d5.setAttribute("src","images/dia/dia5.jpg");
+d5.setAttribute("width","100");
+d5.setAttribute("height","100");
+var d6=document.createElement("img");
+d6.setAttribute("src","images/dia/dia6.jpg");
+d6.setAttribute("width","100");
+d6.setAttribute("height","100");
+var d7=document.createElement("img");
+d7.setAttribute("src","images/dia/dia7.jpg");
+d7.setAttribute("width","100");
+d7.setAttribute("height","100");
+var d8=document.createElement("img");
+d8.setAttribute("src","images/dia/dia8.jpg");
+d8.setAttribute("width","100");
+d8.setAttribute("height","100");
+var d9=document.createElement("img");
+d9.setAttribute("src","images/dia/dia9.jpg");
+d9.setAttribute("width","100");
+d9.setAttribute("height","100");
+var d10=document.createElement("img");
+d10.setAttribute("src","images/dia/dia10.jpg");
+d10.setAttribute("width","100");
+d10.setAttribute("height","100");
+var d11=document.createElement("img");
+d11.setAttribute("src","images/dia/dia11.jpg");
+d11.setAttribute("width","100");
+d11.setAttribute("height","100");
+var d12=document.createElement("img");
+d12.setAttribute("src","images/dia/dia12.jpg");
+d12.setAttribute("width","100");
+d12.setAttribute("height","100");
+var d13=document.createElement("img");
+d13.setAttribute("src","images/dia/dia13.jpg");
+d13.setAttribute("width","100");
+d13.setAttribute("height","100");
+
+<!-- スペードエレメント作成-->
+var s1=document.createElement("img");
+s1.setAttribute("src","images/spade/spade1.jpg");
+s1.setAttribute("width","100");
+s1.setAttribute("height","100");
+var s2=document.createElement("img");
+s2.setAttribute("src","images/spade/spade2.jpg");
+s2.setAttribute("width","100");
+s2.setAttribute("height","100");
+var s3=document.createElement("img");
+s3.setAttribute("src","images/spade/spade3.jpg");
+s3.setAttribute("width","100");
+s3.setAttribute("height","100");
+var s4=document.createElement("img");
+s4.setAttribute("src","images/spade/spade4.jpg");
+s4.setAttribute("width","100");
+s4.setAttribute("height","100");
+var s5=document.createElement("img");
+s5.setAttribute("src","images/spade/spade5.jpg");
+s5.setAttribute("width","100");
+s5.setAttribute("height","100");
+var s6=document.createElement("img");
+s6.setAttribute("src","images/spade/spade6.jpg");
+s6.setAttribute("width","100");
+s6.setAttribute("height","100");
+var s7=document.createElement("img");
+s7.setAttribute("src","images/spade/spade7.jpg");
+s7.setAttribute("width","100");
+s7.setAttribute("height","100");
+var s8=document.createElement("img");
+s8.setAttribute("src","images/spade/spade8.jpg");
+s8.setAttribute("width","100");
+s8.setAttribute("height","100");
+var s9=document.createElement("img");
+s9.setAttribute("src","images/spade/spade9.jpg");
+s9.setAttribute("width","100");
+s9.setAttribute("height","100");
+var s10=document.createElement("img");
+s10.setAttribute("src","images/spade/spade10.jpg");
+s10.setAttribute("width","100");
+s10.setAttribute("height","100");
+var s11=document.createElement("img");
+s11.setAttribute("src","images/spade/spade11.jpg");
+s11.setAttribute("width","100");
+s11.setAttribute("height","100");
+var s12=document.createElement("img");
+s12.setAttribute("src","images/spade/spade12.jpg");
+s12.setAttribute("width","100");
+s12.setAttribute("height","100");
+var s13=document.createElement("img");
+s13.setAttribute("src","images/spade/spade13.jpg");
+s13.setAttribute("width","100");
+s13.setAttribute("height","100");
+
+<!-- エラーエレメント生成 -->
+var e1=document.createElement("img");
+e1.setAttribute("src","images/error.jpg");
+e1.setAttribute("width","100");
+e1.setAttribute("height","100");
+
+<!-- imgエレメント生成ここまで -->
+
+<!-- -->
+var tempi;
+var tempj;
+
+var heart = "♥";
+var clover = "♣";
+var dia = "♦";
+var spade = "♠";
+
+var card=new Array();<!--card配列 -->
+
+<!--配列を作る -->
+for(let i=1;i<14;i++){
+card.push(heart +i);
+card.push(clover +i);
+card.push(dia +i);
+card.push(spade +i);
+}
+
+<!--ajqk配列を作る -->
+var ajqk=new Array();
+for(let i=1;i<14;i++){
+tempi=i;
+if(i==1){
+tempi="A";
+}else if(i==11){
+tempi="J";
+}else if(i==12){
+tempi="Q";
+}else if(i==13){
+tempi="K";
+}
+else{
+tempi=i;
+}
+ajqk.push(heart +tempi);
+ajqk.push(clover +tempi);
+ajqk.push(dia +tempi);
+ajqk.push(spade +tempi);
+}
+
+<!--配列をコピーする -->
+var shufle=ajqk.slice();
+
+<!--shufle:配列をシャッフルする -->
+for(var i=shufle.length-1;i>0;i--){
+var j=Math.floor(Math.random()*(i+1));
+var temp = shufle[i];
+shufle[i]=shufle[j];
+shufle[j]=temp;
+}
+
+<!--shufle配列をコピーする-->
+var copy=shufle.slice();
+<!--img配列を作成する-->
+var elementimg=new Array();
+<!--チェックしてelementimgに格納する -->
+
+for(var c=0; c<copy.length; c++){
+var tempc;
+if(copy[c]=="♥A"){
+tempc=h1;
+}else if(copy[c]=="♥2"){
+tempc=h2;
+}else if(copy[c]=="♥3"){
+tempc=h3;
+}else if(copy[c]=="♥4"){
+tempc=h4;
+}else if(copy[c]=="♥5"){
+tempc=h5;
+}else if(copy[c]=="♥6"){
+tempc=h6;
+}else if(copy[c]=="♥7"){
+tempc=h7;
+}else if(copy[c]=="♥8"){
+tempc=h8;
+}else if(copy[c]=="♥9"){
+tempc=h9;
+}else if(copy[c]=="♥10"){
+tempc=h10;
+}else if(copy[c]=="♥J"){
+tempc=h11;
+}else if(copy[c]=="♥Q"){
+tempc=h12;
+}else if(copy[c]=="♥K"){
+tempc=h13;
+}else if(copy[c]=="♣A"){
+tempc=c1;
+}else if(copy[c]=="♣2"){
+tempc=c2;
+}else if(copy[c]=="♣3"){
+tempc=c3;
+}else if(copy[c]=="♣4"){
+tempc=c4;
+}else if(copy[c]=="♣5"){
+tempc=c5;
+}else if(copy[c]=="♣6"){
+tempc=c6;
+}else if(copy[c]=="♣7"){
+tempc=c7;
+}else if(copy[c]=="♣8"){
+tempc=c8;
+}else if(copy[c]=="♣9"){
+tempc=c9;
+}else if(copy[c]=="♣10"){
+tempc=c10;
+}else if(copy[c]=="♣J"){
+tempc=c11;
+}else if(copy[c]=="♣Q"){
+tempc=c12;
+}else if(copy[c]=="♣K"){
+tempc=c13;
+}else if(copy[c]=="♦A"){
+tempc=d1;
+}else if(copy[c]=="♦2"){
+tempc=d2;
+}else if(copy[c]=="♦3"){
+tempc=d3;
+}else if(copy[c]=="♦4"){
+tempc=d4;
+}else if(copy[c]=="♦5"){
+tempc=d5;
+}else if(copy[c]=="♦6"){
+tempc=d6;
+}else if(copy[c]=="♦7"){
+tempc=d7;
+}else if(copy[c]=="♦8"){
+tempc=d8;
+}else if(copy[c]=="♦9"){
+tempc=d9;
+}else if(copy[c]=="♦10"){
+tempc=d10;
+}else if(copy[c]=="♦J"){
+tempc=d11;
+}else if(copy[c]=="♦Q"){
+tempc=d12;
+}else if(copy[c]=="♦K"){
+tempc=d13;
+}else if(copy[c]=="♠A"){
+tempc=s1;
+}else if(copy[c]=="♠2"){
+tempc=s2;
+}else if(copy[c]=="♠3"){
+tempc=s3;
+}else if(copy[c]=="♠4"){
+tempc=s4;
+}else if(copy[c]=="♠5"){
+tempc=s5;
+}else if(copy[c]=="♠6"){
+tempc=s6;
+}else if(copy[c]=="♠7"){
+tempc=s7;
+}else if(copy[c]=="♠8"){
+tempc=s8;
+}else if(copy[c]=="♠9"){
+tempc=s9;
+}else if(copy[c]=="♠10"){
+tempc=s10;
+}else if(copy[c]=="♠J"){
+tempc=s11;
+}else if(copy[c]=="♠Q"){
+tempc=s12;
+}else if(copy[c]=="♠K"){
+tempc=s13;
+}else{
+tempc=e1;
+}
+elementimg.push(tempc);
+}
+
+<!--shufle配列を整数配列にする -->
+var seisu=new Array();
+for(var j=0; j<shufle.length; j++){
+if(/.A/.test(shufle[j])){
+tempj=1;
+}else if(/.J/.test(shufle[j])){
+tempj=10;
+}else if(/.Q/.test(shufle[j])){
+tempj=10;
+}else if(/.K/.test(shufle[j])){
+tempj=10;
+}else{
+tempj=shufle[j].slice(1);
+}
+seisu.push(tempj);
+}
+
+<!--シャッフルした配列を表示する -->
+function shufle1(){
+document.getElementById("shufle").innerHTML = shufle;
+}
+
+<!--シャッフルした配列を非表示にする -->
+function shufle0(){
+document.getElementById("shufle").innerHTML = "";
+}
+
+var onmore="もう一枚ひきますか？";
+var count=0;
+var psum=0;
+psum+=Number(seisu[count]);
+var inp1="<input type='button' value='はい'onClick='one();'size='50'>";
+var inp2="<input type='button' value='いいえ'onClick='noone();'size='50'>";
+<!--img表示用 -->
+var dispimg; 
+<!--バトル前確認用 -->
+var battlego="<input type='button' value='勝負！'onClick='rio();this.disabled=true;'size='10'>";
+var battleno="<input type='button' value='いいえ'onClick='nono();'size='10'>";
+<!--押せないボタンの準備 -->
+var osenai1="<input type='button' value='はい'disabled='true'size='10'>";
+var osenai2="<input type='button' value='いいえ'disabled='true'size='10'>";
+
+
+
+<!--imgstart:スタートボタンが押された時の処理 -->
+function imgstart(){
+count++;
+psum+=Number(seisu[count]);
+document.getElementById("imgstart").innerHTML = "";
+dispimg=document.getElementById("imgstart");
+dispimg.appendChild(elementimg[0]);
+dispimg.appendChild(elementimg[1]);
+document.getElementById("psum").innerHTML ="合計：" + psum+"<br>"+onmore+inp1+"　"+inp2;
+
+}
+
+<!--inp1の処理 -->
+function one(){
+count++;
+psum+=Number(seisu[count]);
+dispimg.appendChild(elementimg[count]);
+<!--battleを空にしておく -->
+document.getElementById("battle").innerHTML ="";
+document.getElementById("psum").innerHTML ="合計：" + psum+"<br>"+onmore+inp1+"　"+inp2;
+if(psum>=22){
+document.getElementById("psum").innerHTML ="合計：" + psum+"<br>"+onmore+osenai1+"　"+osenai2;
+lose();
+}
+}
+
+<!--inp2の処理 -->
+function noone(){
+<!--押せないボタンにする -->
+document.getElementById("psum").innerHTML ="合計：" + psum+"<br>"+onmore+osenai1+"　"+osenai2;
+document.getElementById("battle").innerHTML ="合計："+psum+"　でリオと勝負しますか？"+battlego+"　"+battleno;
+}
+
+
+<!--バトル前確認 -->
+function nono(){
+document.getElementById("battle").innerHTML = "合計：" + psum+"<br>"+onmore+inp1+"　"+inp2;
+}
+
+//continue:リロード
+
+function con(){
+window.location.reload();
+}
+var coninp="<input type='button' value='もう一回遊ぶ'onClick='con();'size='10'>";
+
+
+
+<!--Loseの処理 -->
+function lose(){
+document.getElementById("result").innerHTML = "Lose・・・";
+document.getElementById("continue").innerHTML = coninp;
+return;
+}
+
+
+
+<!--rioの合計 -->
+function rio(){
+var rio=Math.floor(Math.random()*4+18);
+var battledisp= "あなた："+psum+"　ＶＳ　Rio："+rio;
+var result="";
+if(psum>rio){
+result="Win!!";
+}else if(psum<rio){
+result="Lose・・・";
+}else{
+result="Draw・・";
+}
+document.getElementById("battle").innerHTML = battledisp;
+document.getElementById("result").innerHTML = result;
+document.getElementById("continue").innerHTML = coninp;
+
+}
